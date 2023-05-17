377a1cb6808f87be3f9220a36770b394e8ae5a23
br <learnloop365@gmail.com>
2023-05-17 12:20:42 -0400

    Hello World

---
+++ 1_ Hello World/hw.go
@@ -0,0 +1,18 @@
+/*
+ * Build & Run
+ * e.g.
+ * go build -o build/ hw.go
+ * build/hw
+ *
+ * Quick Run without Full Build Output
+ * e.g.
+ * go run hw.go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
